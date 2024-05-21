573ff90ff32228195603681e5f38d8ff8b51dd02
grindSunday <87099817+grindSunday@users.noreply.github.com>
2024-05-20 20:40:46 -0400

    basic class in working condition. still need to clean data and add methods.

---
+++ custom_libs/requestsCommon.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup as bS
+import requests
+from fake_useragent import UserAgent    # https://pypi.org/project/fake-useragent/#description
+
+
+def get_soup(url, add_user_agent=False, request_type="GET", **kwargs):
+    page = basic_request(url, add_user_agent, request_type, **kwargs)
+    try:
+        content = page.content
+    except:
+        content = page.text
+
+    soup = bS(content, 'html.parser')
+    return soup
+
+
+def basic_request(url, add_user_agent=False, request_type="GET", timeout=5, **kwargs):
+    request_type = request_type
+    headers = {}
+    params = {}
+    proxies = {}
+    if 'type' in kwargs:
+        request_type = kwargs['type']
+    if 'headers' in kwargs:
+        headers = kwargs['headers']
+    if 'params' in kwargs:
+        params = kwargs['params']
+    if 'proxies' in kwargs:
+        proxies = kwargs['proxies']
+    if add_user_agent:
+        random_agent = _add_user_agent_to_header()
+        headers["User-Agent"] = random_agent
+
+    if request_type == "GET":
+        return requests.request(request_type, url, headers=headers, params=params, proxies=proxies, timeout=timeout)
+    else:
+        return requests.post(url, headers=headers, data=params, proxies=proxies, timeout=timeout)
+
+
+def _add_user_agent_to_header():
+    ua = UserAgent(fallback="chrome")
+    return ua.random
+
+
+
+
---
+++ nflTeamStadiums.py
@@ -0,0 +1,71 @@
+from custom_libs import requestsCommon as rC
+from typing import Optional
+
+
+class NFLTeamStadiums:
+    """
+    This class scrapes from wikipedia main page:
+    https://en.wikipedia.org/wiki/List_of_current_NFL_stadiums
+
+    And various pages linked to on the main page.
+    """
+    def __init__(self):
+        self.url = 'https://en.wikipedia.org/wiki/List_of_current_NFL_stadiums'
+        self.data = list()
+        self._get_data()
+
+    def _get_data(self):
+        soup = rC.get_soup(self.url, add_user_agent=True)
+        tables = soup.select('table.wikitable')
+
+        # find main table of page by checking the rows counts and getting table with most rows
+        row_counts = [len(x.find_all('tr')) for x in tables]
+        main_table_index = row_counts.index(max(row_counts))
+        main_table_content = tables[main_table_index].find_all('tr')
+
+        # extract the data
+        columns = [x.text.strip() for x in main_table_content[0].find_all('th')]
+
+        # indices
+        name_index = columns.index('Name')
+        img_index = columns.index('Image')
+        capacity_index = columns.index('Capacity')
+        city_index = columns.index('Location')
+        surface_index = columns.index('Surface')
+        roof_index = columns.index('Roof type')
+        teams_index = columns.index('Team(s)')
+        date_opened_index = columns.index('Opened')
+
+        for row in main_table_content[1:]:
+            cells = row.find_all(['th', 'td'])
+            name = cells[name_index].text.strip()
+            img_url = f"https://en.wikipedia.org/{cells[img_index].find_all('a')[0].attrs['href']}"
+            capacity = int(cells[capacity_index].text.replace(",", "").strip())
+            city = cells[city_index].text.strip()
+            surface = cells[surface_index].text.strip()
+            roof_type = cells[roof_index].text.strip()
+            teams = [x.text for x in cells[teams_index].find_all('a')]
+            year_opened = cells[date_opened_index].text.strip()
+
+            temp_dict = {
+                "name": name,
+                "capacity": capacity,
+                "imgUrl": img_url,
+                "city": city,
+                "surface": surface,
+                "roofType": roof_type,
+                "teams": teams,
+                "yearOpened": year_opened
+                }
+
+            self.data.append(temp_dict.copy())
+
+
+def main():
+    # Test code
+    stds = NFLTeamStadiums()
+    stop = 1
+
+
+if __name__ == '__main__':
+    main()
